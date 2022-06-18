c1a169a31645f0b60bf1c4081cf2f97835d704a2
Jason Shuler <jshuler@gmail.com>
2022-06-17 21:05:38 -0400

    Type hints for LatControl base class

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -2,27 +2,32 @@ from abc import abstractmethod, ABC
 
 from common.numpy_fast import clip
 from common.realtime import DT_CTRL
+from typing import Type
+
+from selfdrive.car.interfaces import CarInterfaceBase
+from selfdrive.controls.lib.vehicle_model import VehicleModel
 
 MIN_STEER_SPEED = 0.3
 
 
 class LatControl(ABC):
-  def __init__(self, CP, CI):
+  def __init__(self, CP, CI: Type[CarInterfaceBase]):
     self.sat_count_rate = 1.0 * DT_CTRL
-    self.sat_limit = CP.steerLimitTimer
+    self.sat_limit: float = CP.steerLimitTimer
     self.sat_count = 0.
 
     # we define the steer torque scale as [-1.0...1.0]
     self.steer_max = 1.0
 
   @abstractmethod
-  def update(self, active, CS, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk):
+  def update(self, active: bool, CS, VM: VehicleModel, params,
+             last_actuators, desired_curvature: float, desired_curvature_rate: float, llk):
     pass
 
   def reset(self):
     self.sat_count = 0.
 
-  def _check_saturation(self, saturated, CS):
+  def _check_saturation(self, saturated: bool, CS) -> bool:
     if saturated and CS.vEgo > 10. and not CS.steeringRateLimited and not CS.steeringPressed:
       self.sat_count += self.sat_count_rate
     else:
